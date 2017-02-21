790dbd4354ee2f017a87be62c137b4f5d18fec8f
urion <urion@1201.us>
2017-02-21 14:48:26 +0800

    modify config

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import NProgress from 'nprogress';
+import { CONFIG } from '../constants/Config.js';
+
+let userGithubUrl = CONFIG.owner;
 
 class Menu extends Component {
   render() {
     return (
       <div id="home">
         <div className="avatar">
-          <a href="https://github.com/cobish"></a>
+          <a href="{{userGithubUrl}}"></a>
         </div>
         <h1>cobish</h1>
         <div className="link">
