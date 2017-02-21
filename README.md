1b784a73b83a48dc1007e7162443360dfb5635fc
urion <urion@1201.us>
2017-02-21 14:32:11 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# urionz's blog
+
+这是一个基于 ``github page`` 和 ``github api`` 搭建的单页面静态博客，点击[「这里」](http://urionz.github.io)查看博客。
---
+++ src/js/constants/Config.js
@@ -0,0 +1,10 @@
+// 配置
+export const CONFIG = {
+  owner      : 'urionz',
+  repo       : 'urionz.github.io',
+  title      : 'urionz - 技术点滴',
+  titleLoad  : 'urionz - 正在加载...',
+  duoshuo    : 'urionz',
+  baiduAnaly : '84602bc3713ca2aeb0258c55adf6a333',
+  // access_token : '6117ebab6cc1cffee7f8d82191c8c937714e05ea'
+};
