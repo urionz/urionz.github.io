32049c935d77681556b18d550f40154790fa9566
urion <urion@1201.us>
2017-02-21 14:37:47 +0800

    modify baidu config

--- src/js/constants/Config.js
+++ src/js/constants/Config.js
@@ -5,6 +5,6 @@ export const CONFIG = {
   title      : 'urionz - 技术点滴',
   titleLoad  : 'urionz - 正在加载...',
   duoshuo    : 'urionz',
-  baiduAnaly : '84602bc3713ca2aeb0258c55adf6a333',
+  baiduAnaly : 'a19b8eed9e761de25974c45f3b8a12b5',
   // access_token : '6117ebab6cc1cffee7f8d82191c8c937714e05ea'
 };
